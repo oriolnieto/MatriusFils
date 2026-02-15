8ae68dd0c86618a4a42e7add529998f75d75e528
vpty <oriolnieto2006@gmail.com>
2026-02-15 18:51:42 +0100

    Implementat seguir, mirar si es pot optimitzar codi. Falta arxius tot

--- src/matrius1/MainMatriu.java
+++ src/matrius1/MainMatriu.java
@@ -70,11 +70,66 @@ public class MainMatriu {
                     }
                     }
 
+                System.out.println();
+
                 for (int l = 0; l < filesMatriu1; l++) {
                     for (int d = 0; d < columnesMatriu2; d++){
                         System.out.print(Resultat[l][d] + " ");
                     }
                     System.out.println();
         }
+                boolean seguir = true;
+                while (seguir) {
+                    System.out.println("Vols seguir amb una altra multiplicació amb les mateixes dimensions o sortir? (S/N)");
+                    String resposta = scan.next();
+                    if (resposta.equals("S")) {
+                        for (int i = 0; i < filesMatriu1; i++) {
+                            for (int j = 0; j < columnesMatriu1; j++){
+                                System.out.println("Introdueix per a la Matriu 1 - Fila " + (i+1) + " Columna " + (j+1) + ":");
+                                Matriu1[i][j] = scan.nextInt();
+                            }
+                        }
+
+                        for (int z = 0; z < filesMatriu2; z++) {
+                            for (int y = 0; y < columnesMatriu2; y++){
+                                System.out.println("Introdueix per a la Matriu 2 - Fila " + (z+1) + " Columna " + (y+1) + ":");
+                                Matriu2[z][y] = scan.nextInt();
+                            }
+                        }
+
+                        FilMatriu[][] fils2 = new FilMatriu[filesMatriu1][columnesMatriu2];
+
+                        for (int o = 0; o < filesMatriu1; o++) {
+                            for (int u = 0; u < columnesMatriu2; u++){
+                                fils2[o][u] = new FilMatriu(Matriu1,Matriu2,o,u,Resultat);
+                                fils2[o][u].start();
+                            }
+                        }
+
+                        for (int y = 0; y < filesMatriu1; y++) {
+                            for (int p = 0; p < columnesMatriu2; p++){
+                                try {
+                                    fils2[y][p].join();
+                                }
+                                catch (InterruptedException e) {
+                                    System.out.println("Error amb els joins.");
+                                }
+                            }
+                        }
+
+                        System.out.println();
+
+                        for (int l = 0; l < filesMatriu1; l++) {
+                            for (int d = 0; d < columnesMatriu2; d++){
+                                System.out.print(Resultat[l][d] + " ");
+                            }
+                            System.out.println();
+                        }
+                    }
+                    else {
+                        seguir = false;
+                        System.exit(0); // es tanca amb codi 0, que vol dir de forma correcta.
+                    }
+                }
     }
 }
